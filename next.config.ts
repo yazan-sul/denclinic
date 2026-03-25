90efea308a9f40ca89c1602ed874ae60fb81d62b
yazan-sul <yazan5sulaiman@gmail.com>
2026-03-25 11:53:32 +0200

    setup  pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
