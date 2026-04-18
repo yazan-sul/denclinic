9c6adc63c2156dc9b49e72e430f963d048a6fc23
Yazan <yazan5sulaiman@gmail.com>
2026-04-18 21:29:17 +0300

    Add Docker Compose configuration and update Prisma migrations

--- prisma/migrations/20260416140104_add_user_email_verified_google_id/migration.sql
+++ prisma/migrations/20260416140104_add_user_email_verified_google_id/migration.sql
@@ -1,6 +1,7 @@
-﻿-- AlterTable
-ALTER TABLE "User" ADD COLUMN     "emailVerified" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "googleId" TEXT;
+-- AlterTable
+ALTER TABLE "User"
+ADD COLUMN "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN "googleId" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
